7ffcefdb4931c1a7a83a894046e0ee396859599b
Kel1 <96129808+kel1x@users.noreply.github.com>
2025-09-09 19:35:38 +0200

    galerija

--- script.js
+++ script.js
@@ -10,5 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
     brojanje++;
     localStorage.setItem("brojPoseta", brojanje);
     document.getElementById("brojac").innerText = brojanje;
+
+    let index = 0;
+    function prikaziSledecu() {
+        let slike = document.querySelectorAll(".slideshow img");
+        for (let i = 0; i < slike.length; i++) {
+            slike[i].style.display = "none";
+        }
+        index++;
+        if (index > slike.length) { index = 1 }
+        slike[index - 1].style.display = "block";
+        setTimeout(prikaziSledecu, 3000);
+    }
+    prikaziSledecu();
 });
 
